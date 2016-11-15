fbdd1b12bc1f0a03358a29454e12f7f40b2fb16f
sqrtofsaturn <sqrtofsaturn@gmail.com>
2016-11-15 16:50:54 -0700

    meh

--- src/csharp/emit-events.cs
+++ src/csharp/emit-events.cs
@@ -11,11 +11,30 @@ using Microsoft.Lync.Model.Extensibility;
 public class Startup
 {
   public Task<object> BindToConversationChanges(Func<object, Task<object>> callback) {
+    System.Console.WriteLine("emit-events:BindToConversationChanges");
 
     var ConversationManager = LyncClient.GetClient().ConversationManager;
 
     ConversationManager.ConversationAdded += (sender, e) => {
-      callback(e);
+      System.Console.WriteLine("emit-events:ConversationAdded");
+      callback("Conversation:Added");
+      System.Console.WriteLine("emit-events:ConversationAdded after callback");
+
+      var conversation = LyncClient.GetClient().ConversationManager.Conversations.FirstOrDefault();
+      var avModality = ((AVModality)conversation.Modalities[ModalityTypes.AudioVideo]);
+
+      avModality.ActionAvailabilityChanged += (sender2, e2) => {
+        System.Console.WriteLine("emit-events:avModality:ActionAvailabilityChanged");
+        callback(e2.Action);
+        System.Console.WriteLine("emit-events:avModality:ActionAvailabilityChanged after callback");
+      };
+
+    };
+
+    ConversationManager.ConversationRemoved += (sender, e) => {
+      System.Console.WriteLine("emit-events:ConversationRemoved");
+      callback("Conversation:Removed");
+      System.Console.WriteLine("emit-events:ConversationRemoved after callback");
     };
 
     return null;
@@ -24,6 +43,8 @@ public class Startup
 
   public async Task<object> Invoke(Func<object, Task<object>> callback)
   {
+    System.Console.WriteLine("emit-events:Invoke");
+
     BindToConversationChanges(callback);
     return null;
   }
