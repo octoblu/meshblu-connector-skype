bb0827bf80a7c764719cc3f29f6cf4746e6a8459
Sqrt Olu and Moheeeeb <serveradmin@octoblu.com>
2016-11-16 12:54:06 -0700

    whatever, erik

--- src/csharp/emit-events.cs
+++ src/csharp/emit-events.cs
@@ -12,21 +12,15 @@ using Microsoft.Lync.Model.Extensibility;
 public class Startup
 {
   public Task<object> BindToVideoChannelChanges(Func<object, Task<object>> callback) {
-    System.Console.WriteLine("emit-events:BindToVideoChannelChanges");
-
     var conversation = LyncClient.GetClient().ConversationManager.Conversations.FirstOrDefault();
     var videoChannel = ((AVModality)conversation.Modalities[ModalityTypes.AudioVideo]).VideoChannel;
 
     videoChannel.ActionAvailabilityChanged += (sender, e) => {
-      System.Console.WriteLine("emit-events:videoChannel:ActionAvailabilityChanged");      
       callback(new KeyValuePair<String, Object>("VideoChannel:ActionAvailabilityChanged", e));
-      System.Console.WriteLine("emit-events:videoChannel:ActionAvailabilityChanged after callback");
     };
 
     videoChannel.StateChanged += (sender, e) => {
-      System.Console.WriteLine("emit-events:videoChannel:StateChanged");
       callback(new KeyValuePair<String, Object>("VideoChannel:StateChanged", e));
-      System.Console.WriteLine("emit-events:videoChannel:StateChanged after callback");
     };
 
     return null;
@@ -40,10 +34,7 @@ public class Startup
     var videoChannelBound = false;
 
     avModality.ActionAvailabilityChanged += (sender, e) => {
-      System.Console.WriteLine("emit-events:avModality:ActionAvailabilityChanged");
       callback(new KeyValuePair<String, Object>("AvModality:ActionAvailabilityChanged:", e));
-      System.Console.WriteLine("emit-events:avModality:ActionAvailabilityChanged after callback");
-
       if ( !videoChannelBound && ((AVModality)sender).CanInvoke(ModalityAction.Connect) ) {
         videoChannelBound = true;
         BindToVideoChannelChanges(callback);
@@ -51,9 +42,7 @@ public class Startup
     };
 
     avModality.ModalityStateChanged += (sender, e) => {
-      System.Console.WriteLine("emit-events:avModality:ModalityStateChanged");
       callback(new KeyValuePair<String, Object>("AvModality:ModalityStateChanged:", e));
-      System.Console.WriteLine("emit-events:avModality:ModalityStateChanged after callback");
     };
 
     return null;
@@ -65,9 +54,7 @@ public class Startup
     var conversation = LyncClient.GetClient().ConversationManager.Conversations.FirstOrDefault();
 
     conversation.StateChanged += (sender, e) => {
-      System.Console.WriteLine("emit-events:conversation:StateChanged");
-      callback(new KeyValuePair<String, Object>("Conversation:StateChanged", e));
-      System.Console.WriteLine("emit-events:conversation:StateChanged after callback");
+      callback(new KeyValuePair<String, Object>("Conversation:StateChanged", null));
     };
 
     return null;
@@ -79,18 +66,13 @@ public class Startup
     var ConversationManager = LyncClient.GetClient().ConversationManager;
 
     ConversationManager.ConversationAdded += (sender, e) => {
-      System.Console.WriteLine("emit-events:ConversationAdded");
-      callback(new KeyValuePair<String, Object>("ConversationManager:ConversationAdded", e));
-      System.Console.WriteLine("emit-events:ConversationAdded after callback");
-
+      callback(new KeyValuePair<String, Object>("ConversationManager:ConversationAdded", null));
       BindToConversationChanges(callback);
       BindToAvModalityChanges(callback);
     };
 
     ConversationManager.ConversationRemoved += (sender, e) => {
-      System.Console.WriteLine("emit-events:ConversationRemoved");
-      callback(new KeyValuePair<String, Object>("ConversationManager:ConversationRemoved", e));
-      System.Console.WriteLine("emit-events:ConversationRemoved after callback");
+      callback(new KeyValuePair<String, Object>("ConversationManager:ConversationRemoved", null));
     };
 
     return null;
