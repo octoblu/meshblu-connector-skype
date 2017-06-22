8f004343c78244d64aaf3a48aae712fffcf9adee
THE CODE SORCERER <sqrtofsaturn@octoblu.com>
2017-06-22 12:40:36 -0700

    added set top most window

--- src/csharp/join-meeting.cs
+++ src/csharp/join-meeting.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using Microsoft.Lync.Model;
 using Microsoft.Lync.Model.Conversation;
 using Microsoft.Lync.Model.Conversation.Sharing;
@@ -11,6 +12,21 @@ using Microsoft.Lync.Model.Extensibility;
 
 public class Startup
 {
+  [DllImport("user32.dll")]
+  [return: MarshalAs(UnmanagedType.Bool)]
+  static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
+
+  static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+
+  const uint SWP_NOSIZE = 0x0001;
+  const uint SWP_NOMOVE = 0x0002;
+  const uint TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+
+  public static void SetTopMostWindow(IntPtr handle)
+  {
+      SetWindowPos(handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
+  }
+
   public Task<ConversationWindow> StartConversation(string joinUrl, long parentHwnd)
   {
     Automation automation = LyncClient.GetAutomation();
@@ -45,6 +61,7 @@ public class Startup
     }
 
     conversationWindow.ShowFullScreen(0);
+    SetTopMostWindow(conversationWindow.Handle);
     return conversationWindow.Conversation.Properties[ConversationProperty.Id].ToString();
   }
 }
