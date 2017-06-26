eca9ff4aac6ec1de42db9f72325ef1eba5118397
Jade Meskill <jade.meskill@gmail.com>
2017-06-26 16:27:35 -0700

    v5.0.16 try something else

--- command.js
+++ command.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+require("node-windows")
 
 const { MeshbluConnectorCommand } = require("meshblu-connector-cli")
 const command = new MeshbluConnectorCommand({ argv: process.argv, connectorPath: __dirname })
