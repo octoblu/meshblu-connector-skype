9418bd83d7070d73d7ea594c0ce2441e94d3f770
ASteinheiser / peterdemartini <andrew@octoblu.com , thepeterdemartini@gmail.com>
2017-08-03 12:16:33 -0700

    v5.1.12 create seperate class for AppLauncher

--- assets/csharp/start-client.cs
+++ assets/csharp/start-client.cs
@@ -5,25 +5,36 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
-public class Startup {
-  public string programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+public static class AppLauncher
+{
+    private static string programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
 
-  public IEnumerable<string> installPaths = new List<string> {
-      @"Microsoft Office\root\Office16\lync.exe",
-      @"Microsoft Office\Office16\lync.exe",
-      @"Microsoft Office\root\Office15\lync.exe",
-      @"Microsoft Office\Office15\lync.exe"
-  }
+    private static IEnumerable<string> installPaths = new List<string> {
+        @"Microsoft Office\root\Office16\lync.exe",
+        @"Microsoft Office\Office16\lync.exe",
+        @"Microsoft Office\root\Office15\lync.exe",
+        @"Microsoft Office\Office15\lync.exe"
+    };
 
-  public IEnumerable<string> paths = installPaths.Select(p => Path.Combine(programFiles, p));
+    public static IEnumerable<string> paths = installPaths.Select(p => Path.Combine(programFiles, p));
 
-  public async Task<object> Invoke(dynamic ignored) {
-    try {
-      var client = paths.First(File.Exists);
-      Process.Start(client);
-    } catch (InvalidOperationException) {
-      throw new Exception("No Lync or Skype client installed");
+    public static void StartClient()
+    {
+        try
+        {
+            var client = paths.First(File.Exists);
+            Process.Start(client);
+        }
+        catch (InvalidOperationException)
+        {
+            throw new Exception("No Lync or Skype client installed");
+        }
     }
+}
+
+public class Startup {
+  public async Task<object> Invoke(dynamic ignored) {
+    AppLauncher.StartClient();
     return null;
   }
 }
